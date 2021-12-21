250d44386b7fc88004c6c3b00b19f22c995a9b4a
George Kent-Scheller <georgeks@bu.edu>
2021-12-21 02:03:09 -0500

    added a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sunglasses for Linkedin
+## Contents
+This program was written to be a background banner for my Linkedin.
+The code fits in a banner sized image and creates an ascii art pair of Sunglasses.
+These shades also have randomized sparkles. 
--- linkedin.cpp
+++ linkedin.cpp
@@ -1,16 +1,16 @@
 #include <iostream>// smaller than an io river
-#include <stdlib.h>// depricated into webmd
-#include <time.h> // in this case .h stand for hour
+#include <stdlib.h>// deprecated into webmd
+#include <time.h> // in this case .h stands for hour
 using namespace std;
 class SuperCoolClass{ public: SuperCoolClass();SuperCoolClass(int sg){SunglassBrightness = sg;}
-	int SunglassBrightness =98; 
-	string glasses="                r===============/|\n               /               /.|\n              /               /..|\n             /              /||..j\n            /              /.||./\n           /              /..||/\n          /              /...j\n    r===================|.../\n   /    /               |../\n  /    /b               |./\n /                      |/\n/b\n"; ;
+    int SunglassBrightness =98; 
+    string glasses="                r===============/|\n               /               /.|\n              /               /..|\n             /              /||..j\n            /              /.||./\n           /              /..||/\n          /              /...j\n    r===================|.../\n   /    /               |../\n  /    /b               |./\n /                      |/\n/b\n"; ;
     unsigned int GlassLocations [18] = {67,101,102,136,137,168,171,201,202,233,234,235,263,264,265,293,294,322};
-	void RockDemSpecks(){
-	 int ind =0;srand(time(NULL));for (unsigned int i =0;i<glasses.size();i++){
-	  float dassle = rand()%100;if(i==GlassLocations[ind]){//     r===============/|	
-	   ind++;glasses[i]=(dassle<SunglassBrightness)?'*':' ';}//  /               /*|
-	 }cout<<glasses;}};// ----------->https://github.com/pequode/stayIcy<----   /**|
+    void RockDemSpecks(){
+      int ind =0;srand(time(NULL));for (unsigned int i =0;i<glasses.size();i++){
+      float dassle = rand()%100;if(i==GlassLocations[ind]){//     r===============/|	
+       ind++;glasses[i]=(dassle<SunglassBrightness)?'*':' ';}//  /               /*|
+     }cout<<glasses;}};// ----------->https://github.com/pequode/stayIcy<----   /**|
 int main(){//                                                  /              /||**j
  SuperCoolClass Fonze(90);//                                  /              / ||*/
  Fonze.RockDemSpecks();//                                    /              /**||/
